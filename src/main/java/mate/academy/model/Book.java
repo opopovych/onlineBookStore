11a72fe2087f4d39ad669c0fc1774a6285ff4c13
Oleh <popovich158@gmail.com>
2023-10-19 00:45:24 +0300

    added model book, and repo with service.

---
+++ src/main/java/mate/academy/model/Book.java
@@ -0,0 +1,30 @@
+package mate.academy.model;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Data
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Nonnull
+    private String title;
+    @Nonnull
+    private String author;
+    @Nonnull
+    @Column(unique = true)
+    private String isbn;
+    @Nonnull
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+
+}
