ee0c239b29a59a17779a057876553f10808b79d8
Oleh <popovich158@gmail.com>
2023-10-21 18:34:00 +0300

    Created RequestMapping annotation in controller.

--- src/main/java/mate/academy/controller/BookController.java
+++ src/main/java/mate/academy/controller/BookController.java
@@ -5,28 +5,25 @@ import mate.academy.dto.BookDto;
 import mate.academy.dto.CreateBookRequestDto;
 import mate.academy.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/books")
 public class BookController {
     @Autowired
     private BookService bookService;
 
-    @GetMapping("api/books")
+    @GetMapping
     public List<BookDto> getAll() {
         return bookService.findAll();
     }
 
-    @GetMapping("api/books/{id}")
+    @GetMapping("/{id}")
     public BookDto getById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
-    @PostMapping("api/books")
+    @PostMapping
     public BookDto createBook(@RequestBody CreateBookRequestDto createBookRequestDto) {
         return bookService.save(createBookRequestDto);
     }
