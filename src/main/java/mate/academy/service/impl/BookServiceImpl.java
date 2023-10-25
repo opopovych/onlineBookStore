6c39c2292b2ec5a787b21ff5d777c01a38aa59d9
Oleh <popovich158@gmail.com>
2023-10-26 01:54:04 +0300

    implemented this endpoint: GET: /api/books (Retrieve book catalog) GET: /api/books/{id} (Retrieve book details) POST: /api/books (Create a new book) PUT: /api/books/{id} (Update a specific book) DELETE /api/books/{id} (Delete a specific book) GET: /api/books/search (search book by parameters)

--- src/main/java/mate/academy/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/service/impl/BookServiceImpl.java
@@ -1,14 +1,18 @@
 package mate.academy.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 import mate.academy.dto.BookDto;
+import mate.academy.dto.BookSearchParametersDto;
 import mate.academy.dto.CreateBookRequestDto;
 import mate.academy.exception.EntityNotFoundException;
 import mate.academy.mapper.BookMapper;
 import mate.academy.model.Book;
-import mate.academy.repository.BookRepository;
+import mate.academy.repository.book.BookRepository;
+import mate.academy.repository.book.BookSpecificationBuilder;
 import mate.academy.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +21,9 @@ public class BookServiceImpl implements BookService {
     private BookMapper bookMapper;
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private BookSpecificationBuilder bookSpecificationBuilder;
 
     @Override
     public BookDto save(CreateBookRequestDto book) {
@@ -37,5 +44,40 @@ public class BookServiceImpl implements BookService {
         Book book = bookRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Can't find book with id - " + id));
         return bookMapper.toDto(book);
+    }
+
+    @Override
+    public BookDto removeById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find book with id - " + id));
+        bookRepository.delete(book);
+        return bookMapper.toDto(book);
+    }
+
+    @Override
+    public BookDto update(Long id, CreateBookRequestDto updateBookRequestDto) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            Book existingBook = optionalBook.get();
+            existingBook.setTitle(updateBookRequestDto.getTitle());
+            existingBook.setAuthor(updateBookRequestDto.getAuthor());
+            existingBook.setIsbn(updateBookRequestDto.getIsbn());
+            existingBook.setPrice(updateBookRequestDto.getPrice());
+            existingBook.setDescription(updateBookRequestDto.getDescription());
+            existingBook.setCoverImage(updateBookRequestDto.getCoverImage());
+            Book updatedBook = bookRepository.save(existingBook);
+            return bookMapper.toDto(updatedBook);
+        } else {
+            throw new RuntimeException("Book not found");
+        }
+    }
+
+    @Override
+    public List<BookDto> search(BookSearchParametersDto searchParameters) {
+        Specification<Book> bookSpecification = bookSpecificationBuilder.build(searchParameters);
+        return bookRepository.findAll(bookSpecification)
+                .stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 }
