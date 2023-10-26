002eac533835ea72c6a83a2b6559f8fa6501cfc9
Oleh <popovich158@gmail.com>
2023-10-27 00:30:07 +0300

    Added CustomGlobalExceptionHandler class Added meaningful validation for existing DTO classes

--- src/main/java/mate/academy/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/dto/CreateBookRequestDto.java
@@ -1,13 +1,20 @@
 package mate.academy.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
+    @NotNull
     private String isbn;
     private String description;
     private String coverImage;
