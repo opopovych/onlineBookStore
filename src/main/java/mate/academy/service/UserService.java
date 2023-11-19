6335b91e5928e21c1bc9123d370a1008a59efaee
Oleh <popovich158@gmail.com>
2023-11-19 17:58:04 +0200

    all done

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -2,8 +2,7 @@ package mate.academy.service;
 
 import mate.academy.dto.UserRegistrationRequestDto;
 import mate.academy.dto.UserResponseDto;
-import mate.academy.exception.RegistrationException;
 
 public interface UserService {
-    UserResponseDto register(UserRegistrationRequestDto request) throws RegistrationException;
+    UserResponseDto register(UserRegistrationRequestDto request);
 }
