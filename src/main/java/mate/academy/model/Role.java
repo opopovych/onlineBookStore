abef95a4267fbee71698b57f854e5c17d27f8059
Oleh <popovich158@gmail.com>
2024-01-14 19:22:37 +0200

    updated used mentor's comments. added @ISBN removed empty lines

--- src/main/java/mate/academy/model/Role.java
+++ src/main/java/mate/academy/model/Role.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.GrantedAuthority;
 @Data
 @Table(name = "roles")
 public class Role implements GrantedAuthority {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
