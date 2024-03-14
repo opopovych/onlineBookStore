8d1a8f5742e4b9ea6d58a58d56772eda70dabfb8
Oleh <popovich158@gmail.com>
2024-03-14 22:35:13 +0200

    added test class

---
+++ src/test/java/mate/academy/BookStoreApplicationTests.java
@@ -0,0 +1,12 @@
+package mate.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+    // There something went wrong
+    @Test
+    void contextLoads() {
+    }
+}
