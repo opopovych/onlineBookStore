3fb8e991671a4ae1ef917809d872636be06c6aaf
Oleh <popovich158@gmail.com>
2024-03-14 22:55:07 +0200

    removed spring test annotation.

--- src/test/java/mate/academy/BookStoreApplicationTests.java
+++ src/test/java/mate/academy/BookStoreApplicationTests.java
@@ -1,9 +1,7 @@
 package mate.academy;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BookStoreApplicationTests {
     // There something went wrong
     @Test
