1b46efacaf613c99d598473e0f3d013aeee16805
Oleh <popovich158@gmail.com>
2024-03-15 20:11:42 +0200

    deleted all additional local files, and returned spring test annotation.

--- src/test/java/mate/academy/BookStoreApplicationTests.java
+++ src/test/java/mate/academy/BookStoreApplicationTests.java
@@ -1,7 +1,9 @@
 package mate.academy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class BookStoreApplicationTests {
     // There something went wrong
     @Test
