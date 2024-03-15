c2d6fe03ecc943483500f6a6181a2e1270e87c2b
Oleh <popovich158@gmail.com>
2024-03-15 21:46:42 +0200

    Updated method in findByCategoryId. Added authority to ShoppingCartController.

--- src/main/java/mate/academy/controller/ShoppingCartController.java
+++ src/main/java/mate/academy/controller/ShoppingCartController.java
@@ -9,6 +9,7 @@ import mate.academy.dto.cartitem.UpdateCartItemRequestDto;
 import mate.academy.dto.shoppingcart.ShoppingCartDto;
 import mate.academy.model.User;
 import mate.academy.service.ShoppingCartService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShoppingCartController {
     private final ShoppingCartService shoppingCartService;
 
+    @PreAuthorize("hasAuthority('USER')")
     @Operation(summary = "Get the shopping cart",
             description = "Get user's shopping cart, show what is in it")
     @GetMapping
@@ -34,6 +36,7 @@ public class ShoppingCartController {
         return shoppingCartService.getCartByUserId(user.getId());
     }
 
+    @PreAuthorize("hasAuthority('USER')")
     @Operation(summary = "Add books to the shopping cart",
             description = "Add books to the user's shopping cart")
     @PostMapping
@@ -43,6 +46,7 @@ public class ShoppingCartController {
         return shoppingCartService.addBookToCartByUserId(user.getId(), requestDto);
     }
 
+    @PreAuthorize("hasAuthority('USER')")
     @Operation(summary = "Update cart item",
             description = "Update cart item in the shopping cart")
     @PutMapping("/cart-items/{cartItemId}")
@@ -53,6 +57,7 @@ public class ShoppingCartController {
         return shoppingCartService.updateCartItemById(user.getId(), cartItemId, requestDto);
     }
 
+    @PreAuthorize("hasAuthority('USER')")
     @Operation(summary = "Delete a cart item",
             description = "Delete a specific item from the shopping cart")
     @DeleteMapping("/cart-items/{cartItemId}")
