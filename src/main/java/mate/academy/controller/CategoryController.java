882f847a9fb2182d36e5cea8c9067510620954ea
Oleh <popovich158@gmail.com>
2024-04-23 20:08:21 +0300

    added readme file and updated some classes.

--- src/main/java/mate/academy/controller/CategoryController.java
+++ src/main/java/mate/academy/controller/CategoryController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/categories")
+@RequestMapping("/categories")
 public class CategoryController {
 
     private final CategoryService categoryService;
